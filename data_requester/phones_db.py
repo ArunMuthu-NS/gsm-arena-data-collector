d59624f75300b603f63d4f8ed69b2fd2800fc1d5
Arunkumar Muthusamy <arun@Arunkumars-MBP.lan>
2021-12-30 12:36:33 -0500

    Adds phones data write to file

--- data_requester/phones_db.py
+++ data_requester/phones_db.py
@@ -5,3 +5,9 @@ def load_processed_phone_links(file_name):
             phones_list.append(phone.strip())
 
     return phones_list
+
+
+def store_phone_names(processed_phone_links, file_name):
+    with open(file_name, "a") as phone_file:
+        for phone in processed_phone_links:
+            phone_file.write(phone + "\n")
--- tests/test_phones_db.py
+++ tests/test_phones_db.py
@@ -1,15 +1,25 @@
-from data_requester.phones_db import load_processed_phone_links
+from data_requester.phones_db import load_processed_phone_links, store_phone_names
 
 
 def simulate_loaded_phone_data(tmp_path):
-    d = tmp_path / "temp"
+    d = tmp_path / 'temp'
     d.mkdir()
-    p = d / "temp.txt"
-    p.write_text("a.php\nb.php")
+    p = d / 'temp.txt'
+    p.write_text('a.php\nb.php\n')
     return p
 
 
 def test_load_phone_names_returns_saved_phone_names(tmp_path):
     expected = ['a.php', 'b.php']
     actual = load_processed_phone_links(simulate_loaded_phone_data(tmp_path))
+    assert actual == expected
+
+
+def test_store_phone_names_get_saved(tmp_path):
+    file_path = simulate_loaded_phone_data(tmp_path)
+    store_phone_names(['c.php', 'd.php'], file_path)
+    expected = ['a.php', 'b.php', 'c.php', 'd.php']
+
+    actual = load_processed_phone_links(file_path)
+
     assert actual == expected
